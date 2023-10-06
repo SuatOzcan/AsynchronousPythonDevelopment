d7aa4f0ce7cea0b3ae8ac3b1a1c348497c0dc235
Suat <41187131+SuatOzcan@users.noreply.github.com>
2023-10-06 14:47:50 +0300

    Apply queues to use with threads.

--- queued_threads.py
+++ queued_threads.py
@@ -14,5 +14,28 @@ def increment_manager():
         increment = counter_queue.get() #This waits until an item is available, and then locks the queue.
         old_counter = counter
         counter = old_counter + increment
-        job_queue.put(f'New counter value is {counter}', '-------')
+        job_queue.put((f'New counter value is {counter}','-------------'))
         counter_queue.task_done() #This unlocks the counter_queue.
+    
+Thread(target=increment_manager, daemon=True).start()
+
+def printer_manager():
+    while True:
+        for line in job_queue.get():
+            print(line)
+        job_queue.task_done()
+
+Thread(target=printer_manager, daemon=True).start()
+
+def increment_counter():
+    counter_queue.put(1)
+
+worker_threads = [Thread(target = increment_counter) for thread in range(10)]
+for thread in worker_threads:
+    thread.start()
+
+# for thread in worker_threads:
+#     thread.join()
+
+counter_queue.join()
+job_queue.join()
