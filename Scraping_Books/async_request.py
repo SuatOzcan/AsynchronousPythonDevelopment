8fe18ffd427e6cdb04805db43bd410fe0f4b90c1
Suat <41187131+SuatOzcan@users.noreply.github.com>
2023-10-12 09:50:12 +0300

    Update async_request.py
    It now connects with a single client session instead of creating a session each time.

--- Scraping_Books/async_request.py
+++ Scraping_Books/async_request.py
@@ -2,17 +2,27 @@ import aiohttp
 import asyncio  
 import time
 
-async def fetch_page(url):
+async def fetch_page(session, url):
     page_start = time.time()
-    async with aiohttp.ClientSession() as session:
+    # async with aiohttp.ClientSession() as session:
         #Methods with an async keyword may have a yield method in __enter__, __exit__, or both methods.
         #New methods are __aenter__ and __aexit__.
-        async with session.get(url) as response:
-            print(f'Page took {time.time() - page_start} seconds to complete.')
-            return response.status
+    async with session.get(url) as response:
+        print(f'Page took {time.time() - page_start} seconds to complete.')
+        return response.status
+        
+async def get_multiple_pages(loop, *urls):
+    tasks = []
+    async with aiohttp.ClientSession(loop = loop) as session:
+        for url in urls:
+            tasks.append(fetch_page(session, url))
+        grouped_tasks = asyncio.gather(*tasks)
+        return await grouped_tasks
+        # await grouped_tasks
+        # return grouped_tasks
 
 loop = asyncio.get_event_loop()
-tasks = [fetch_page('http://www.google.com') for i in range(50)]
+urls = ['https://www.google.com/' for i in range(50)]
 start = time.time()
-loop.run_until_complete(asyncio.gather(*tasks))
+loop.run_until_complete(get_multiple_pages(loop, *urls))
 print(f'All took {time.time() - start} seconds to complete.')
